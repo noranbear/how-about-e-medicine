6b63a01a936a6e238c6eab35cd18be182b2183de
Sohee Kim <noranbear0210@gmail.com>
2022-07-05 15:59:24 +0900

    🎉 프로젝트 환경설정 완료

---
+++ src/main/java/com/multi/Medicine2Application.java
@@ -0,0 +1,13 @@
+package com.multi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Medicine2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Medicine2Application.class, args);
+	}
+
+}
