6b3997381c2111dfd7400e117528c4e9cb21ceda
Sohee Kim <noranbear0210@gmail.com>
2022-07-14 12:25:55 +0900

    ✨ ✅ OCR API-envelope 연동 및 테스트 완료

---
+++ src/main/java/com/multi/controller/AJAXController.java
@@ -0,0 +1,35 @@
+package com.multi.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.multi.restapi.OCREnvelopeAPI;
+
+/**
+ * @author noranbear
+ * @date 2022. 7. 14.
+ * @version 1.0
+ * @description
+ *
+ *
+ * =========================================================
+ * 	    DATE			 AUTHOR				    NOTE
+ * ---------------------------------------------------------
+ *  2022. 7. 14.		noranbear		   evelopescan 생성
+ *
+ * =========================================================
+ */
+@RestController
+public class AJAXController {
+	
+	@Autowired
+	OCREnvelopeAPI eapi;
+	
+	@RequestMapping("envelopescan")
+	public Object envelopescan(String imgname) {
+		Object result = eapi.envelopeapi(imgname);
+		return result;
+	}
+
+}
