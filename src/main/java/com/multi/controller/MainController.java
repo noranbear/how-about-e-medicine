4dc678ee7811ecc26f9e4306d690414133d824d3
Sohee Kim <noranbear0210@gmail.com>
2022-07-15 20:06:15 +0900

    💄 Bootstrap 장착 및 화면 개발 준비를 위한 기본 페이지 연동

--- src/main/java/com/multi/controller/MainController.java
+++ src/main/java/com/multi/controller/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 /**
  * @author noranbear
  * @date 2022. 7. 6.
- * @version 1.0
+ * @version 2.0
  * @description
  *
  *
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * ---------------------------------------------------------
  *  2022. 7. 6.			noranbear			  main 생성
  *
+ *	2022. 7. 15								dashboard 생성
+ *											sign in 생성
  * =========================================================
  */
 
@@ -22,11 +24,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MainController {
 	
 	/**
-	 * 메인 페이지(index) 연결
+	 * 메인 페이지 연결
 	 * @return index
 	 */
 	@RequestMapping("/")
 	public String main() {
 		return "index";
+	}
+	
+	/**
+	 * 대시보드 페이지 연결
+	 * @return dashboard.html
+	 */
+	@RequestMapping("/dashboard")
+	public String dashboard() {
+		return "dashboard";
+	}
+	
+	/**
+	 * 로그인 페이지 연결
+	 * @return signin.html
+	 */
+	@RequestMapping("/signin")
+	public String signin() {
+		return "signin";
+	}
+	
+	/**
+	 * 회원가입 페이지 연결
+	 * @return signup.page
+	 */
+	@RequestMapping("/signup")
+	public String signup() {
+		return "signup";
 	}
 }
